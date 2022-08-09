b88b9db62cceae1f86e8fa6de3c7aa853e8af295
Josh Rae <joshuar2155@gmail.com>
2022-08-09 18:39:32 -0400

    sample index.js, readme

--- index.js
+++ index.js
@@ -11,3 +11,35 @@ function init() {}
 
 // Function call to initialize app
 init();
+
+
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?',
+    },
+    {
+      type: 'checkbox',
+      message: 'What languages do you know?',
+      name: 'stack',
+      choices: ['HTML', 'CSS', 'JavaScript', 'MySQL'],
+    },
+    {
+      type: 'list',
+      message: 'What is your preferred method of communication?',
+      name: 'contact',
+      choices: ['email', 'phone', 'telekinesis'],
+    },
+  ])
+  .then((data) => {
+    const filename = `${data.name.toLowerCase().split(' ').join('')}.json`;
+
+    fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
+      err ? console.log(err) : console.log('Success!')
+    );
+  });
